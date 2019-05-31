103b6435b55d04edf211f03801c78a7f6dd1a150
kurlee <adrianabdala1989@gmail.com>
2019-05-31 11:59:07 +0000

    Change dir name

---
+++ MarksmanWitness/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This script will be used in the main menu to select scenes to load
+/// by clicking a button
+/// 
+/// Author: Ben Hoffman
+/// </summary>
+public class MainMenu : MonoBehaviour {
+
+    public Canvas aboutCanvas;
+
+    private void Start()
+    {
+        aboutCanvas.gameObject.SetActive(false);
+    }
+
+    public void ToggleAboutCanvas()
+    {
+        aboutCanvas.gameObject.SetActive(!aboutCanvas.gameObject.activeInHierarchy);
+    }
+
+    public void OpenWebsite()
+    {
+        Application.OpenURL("http://ergowitness.info");
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);     
+    }
+
+    public void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
